227042bc49b4d4195bf5c44d8daa446f91dbbacc
Ajitsingh Arjunsingh Chauhan <113981559+ajit200315@users.noreply.github.com>
2025-06-16 01:23:49 -0700

    Install NPM now vite is updating and git is also tracking files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-    <p className='bg-pink-700'>Hello there</p>
+    <p className='bg-pink-700'>d</p>
     </>
   )
 }
