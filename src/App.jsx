9f9efdf19cbf9970794bd3858aa7068d7226953a
Ajitsingh Arjunsingh Chauhan <113981559+ajit200315@users.noreply.github.com>
2025-06-17 16:06:18 -0700

    Added Image on the Homepage which is also aligned with the theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
-import './App.css'
-import { Background } from './Component/Background'
+import "./App.css";
+import { Background } from "./Component/Background";
 import GlassmorphicNavbar from "./Component/Navbar";
 
-
 function App() {
- 
-
   return (
     <>
-    <body className="bg-black text-white min-h-screen">
+      <body className="bg-black text-white min-h-screen">
+        <div className="p-9">
+          <p className="text-5xl absolute top-9 left-9">Ajit.</p>
+          <GlassmorphicNavbar />
+        </div>
 
-      <div className='p-9'>
-        <p className='text-5xl absolute top-9 left-9'>Ajit.</p>
-      <GlassmorphicNavbar />
-      </div>
-
-      <Background/>
+        <div>
+        <img src="Photos\picture.png" alt="My Profile Picture"  className="mix-blend-lighten w-192 md:w-384 lg:w-170 absolute left-230 bottom-8"/>
+        </div>
 
 
+        <Background />
       </body>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
