993e7c2be2836208a25c812b474f417202e3184b
Ajitsingh Arjunsingh Chauhan <113981559+ajit200315@users.noreply.github.com>
2025-06-19 20:00:38 -0700

    Config for Deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()],
-  base: '/your-repository-name/', 
+  base: 'AjitPortfolio', 
   build: {
     outDir: 'dist'
   }
